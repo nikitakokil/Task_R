81e0ed0766c5a3fafafcadd7a93164c6bd967e84
nikitakokil <137026704+nikitakokil@users.noreply.github.com>
2024-03-24 11:12:41 +0530

    Add files via upload

---
+++ Task_R.R
@@ -0,0 +1,3 @@
+# Assuming your dataset is in a CSV file named "dataset.csv" in the working directory
+data <- read.csv("C:\\Users\\LENOVO\\Desktop\\LearnTube Assignments\\Dataset - Data Science - Task 5.csv")
+summary(data)
